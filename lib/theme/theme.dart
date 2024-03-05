49a86bb6e4c79a505dd705f826fbde028a8fac82
Taassneem <tasnemeadwan87@gmail.com>
2024-03-05 23:37:45 +0000

    and new text size

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 ThemeData getThemeData() {
   return ThemeData(
       textTheme: const TextTheme(
-    titleLarge: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-    titleMedium: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-    titleSmall: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
-  ));
+          titleLarge: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+          titleMedium: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+          titleSmall: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+          headlineLarge: TextStyle(fontSize: 48, fontWeight: FontWeight.w800)));
 }
